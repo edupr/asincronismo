39a979d8d37fe2799552fea3ee5623dc9044d9d8
Edu <particularspam@gmail.com>
2022-06-29 00:27:58 -0500

    Promise

---
+++ src/promise/index.js
@@ -0,0 +1,40 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey1!');
+        } else {
+            reject('Whoooops1!');
+        }
+    });
+};
+
+somethingWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error('Whooops2!');
+            reject(error);
+        }
+    });
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+
+
+// Correr varias Promesas al mismo tiempo:
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
